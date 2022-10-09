80b62e4d65abfc5d2a8e5c3d54fe512623c102af
Eslam Hussien Sallam <31744922+EslamSallam@users.noreply.github.com>
2022-10-09 12:48:46 +0200

    Creating DbContext Class

---
+++ FriendOrganizer.DataAccess/FriendOrganizerDBContext.cs
@@ -0,0 +1,27 @@
+﻿using FriendOrganizer.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendOrganizer.DataAccess
+{
+    public class FriendOrganizerDBContext : DbContext
+    {
+        public DbSet<Friend> Friends { get; set; }
+        public FriendOrganizerDBContext() : base("FriendOrganizerDb")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
